52aa5401706cfd5930e5d3283983c32fbcb0d88c
Samuel Dalesjö <Dalesjo@users.noreply.github.com>
2022-07-21 13:39:32 +0200

    example on filtering streamnames (BUT will not work because we cannot distinguish broadcasters/players)

--- src/com/dalesjo/wowza/WebRTCAuthentication.java
+++ src/com/dalesjo/wowza/WebRTCAuthentication.java
@@ -7,40 +7,55 @@ import com.wowza.wms.application.*;
 import com.wowza.wms.module.*;
 import com.wowza.wms.rtp.model.*;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 
 public class WebRTCAuthentication extends ModuleBase {
 
 	public void onAppStart(IApplicationInstance appInstance) {
+
 		String fullname = appInstance.getApplication().getName() + "/" + appInstance.getName();
+
 		info("onAppStart: " + fullname);
+
+		var properties = appInstance.getProperties();
+
+		parameter = getValueOrDefault((String) properties.get(parameterProperty), "token");
+		secret = (String) properties.get(secretProperty);
+		subjectOnly = (Boolean) properties.get(subjectProperty);
+		
+		if (secret == null) {
+			warning("No secret have been set application will not validate any tokens.");
+		}
 	}
+
+	private static final String secretProperty = "dalesjo-webrtc-secret";
+	private static final String parameterProperty = "dalesjo-webrtc-parameter";
+	private static final String subjectProperty = "dalesjo-webrtc-subject-only";
+
+	private String parameter = null;
+	private Boolean subjectOnly = false;
+	private String secret = null;
 
 	public void onRTPSessionCreate(RTPSession rtpSession) {
 		info("onRTPSessionCreate: " + rtpSession.getSessionId());
-		
+
 		try {
-			
-			if(!rtpSession.isWebRTC())
-			{
+
+			if (!rtpSession.isWebRTC()) {
 				return;
 			}
-			
-			
-			
-			var key = "token";
-			var secret = "secret";
-			var streamName = rtpSession.getRTSPStream().getStreamName();
-			
-			info("streamname:"+ streamName);
-			
-			Map<String, Object> userData = (Map<String, Object>)rtpSession.getWebRTCSession().getCommandRequest().getJSONEntries().get("userData");
-			String token = (String)userData.get(key);
-			
+
+			if (secret == null) {
+				return;
+			}
+
+			var userData = (Map<String, Object>) rtpSession.getWebRTCSession().getCommandRequest().getJSONEntries().get("userData");
+			var token = (String) userData.get(parameter);
 			var claims = Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
 			
-
+			//checkStreamName(rtpSession,claims);
 			
 		} catch (Exception e) {
 			info("Rejecting connection, exception:" + e.getMessage());
@@ -48,12 +63,31 @@ public class WebRTCAuthentication extends ModuleBase {
 		}
 	}
 
+	private void checkStreamName(RTPSession rtpSession,Claims claims) throws Exception
+	{
+		var streamName = rtpSession.getRTSPStream().getStreamName();
+		var subject = claims.getSubject();
+		
+		if(streamName != subject)
+		{
+			throw new Exception("Subject "+ subject + " is trying to stream "+ streamName +" which is forbidden");
+		}
+		
+	}
+	
 	public void onRTPSessionDestroy(RTPSession rtpSession) {
 		info("onRTPSessionDestroy: " + rtpSession.getSessionId());
 	}
-	
-	  private void info(String message)
-	  {
-		  getLogger().info("WebRTCAuthentication: " + message);
-	  }
+
+	private void info(String message) {
+		getLogger().info("WebRTCAuthentication: " + message);
+	}
+
+	private void warning(String message) {
+		getLogger().warn("WebRTCAuthentication: " + message);
+	}
+
+	public static <T> T getValueOrDefault(T value, T defaultValue) {
+		return value == null ? defaultValue : value;
+	}
 }
